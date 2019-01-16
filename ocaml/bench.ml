e226b9922d04a78cac1501714176188a3d3e3560
Pieter Goetschalckx <3.14.e.ter@gmail.com>
2019-01-16 15:25:32 +0100

    New OCaml version without polymorphic compare and mutability

--- ocaml/bench.ml
+++ ocaml/bench.ml
@@ -1,3 +1,11 @@
+module Float = struct
+  let min (x : float) (y : float) =
+    if x < y then x else y
+
+  let max (x : float) (y : float) =
+    if x > y then x else y
+end
+
 module Rectangle = struct
   type t = {
     lx: float;
@@ -6,21 +14,17 @@ module Rectangle = struct
     ty: float
   }
 
-  let r lx by rx ty = {lx = lx; by = by; rx = rx; ty = ty}
+  let r lx by rx ty = {lx; by; rx; ty}
 
-  let union {lx = alx; by = aby; rx = arx; ty = aty}
-            {lx = blx; by = bby; rx = brx; ty = bty} =
-    r (min alx blx)
-      (min aby bby)
-      (max arx brx)
-      (max aty bty)
-end;;
+  let union {lx; by; rx; ty} {lx = lx'; by = by'; rx = rx'; ty = ty'} =
+    r (Float.min lx lx') (Float.min by by') (Float.max rx rx') (Float.max ty ty')
 
-open Rectangle
+  let print {lx; by; rx; ty} =
+    Printf.printf "{lx=%f; by=%f; rx=%f; ty=%f}\n" lx by rx ty
+end
 
-let lim = 1000000000
-let _ = let rect = ref @@ r 25. 25. 200. 200. in
-        for i = 0 to lim do
-          rect := union !rect @@ r 20. 0. 100. (float_of_int i)
-        done;
-        print_endline @@ string_of_float @@ !rect.ty
+let () =
+  let rec loop rect = function
+    | 0 -> Rectangle.print rect
+    | i -> loop (Rectangle.union rect (Rectangle.r 20. 0. 100. (float_of_int i))) (i - 1) in
+  loop (Rectangle.r 25. 25. 200. 200.) 1000000000
